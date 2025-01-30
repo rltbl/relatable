4317b205444676b8ea2d289d349ea4f5804e05f7
James A. Overton <james@overton.ca>
2025-01-30 16:50:06 -0500

    Make pollData respect filters

--- rltbl-frontend/src/Grid.tsx
+++ rltbl-frontend/src/Grid.tsx
@@ -292,7 +292,11 @@ export default function Grid(grid_args: { rltbl: any, height: number }) {
   // Fetch data updated since we started.
   const pollData = React.useCallback(async () => {
     if (!dataRef.current) { return; }
-    const url = `${site.root}/table/${table}.json?_change_id=gt.${change_id.current}`;
+    const params = new URLSearchParams(document.location.search);
+    params.set("_change_id", `gt.${change_id.current}`);
+    params.delete("limit");
+    params.delete("offset");
+    const url = `${site.root}/table/${table}.json?${params.toString()}`;
     // console.log("Fetch: " + url);
     var rows: Row[] = [];
     try {
